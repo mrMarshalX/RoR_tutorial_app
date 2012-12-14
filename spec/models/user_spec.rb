dfad3f74f2e0bd413360cd8f1c7717fffaea7bd8
Marcin Strazynski <strazynski@gmail.com>
2012-12-14 16:05:07 +0100

    Finish user edit, update, index and destroy actions from scratch

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,8 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -29,7 +31,16 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it {should_not be_admin }
 
+  # ADMIN
+  describe "with the admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it { should be_admin } 
+  end
 
   # NAMES
   describe "when name is not present" do
